f2e731f6c351bc9423022cced0a7c0bee73091ab
grifb <114772878+grifb@users.noreply.github.com>
2024-09-28 12:12:00 -0400

    chore: initial commit

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+import express, { json, urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+
+const app = express();
+const port = 3000;
+
+app.use(logger('dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <h1 className="font-bold text-2xl">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
