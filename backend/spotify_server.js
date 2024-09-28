d78692d7dec9c2ab0a574795b80f6199e849a3e6
grifb <114772878+grifb@users.noreply.github.com>
2024-09-28 13:47:42 -0400

    feat: added function to get my access token

--- backend/spotify_server.js
+++ backend/spotify_server.js
@@ -14,6 +14,25 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 
+// This function will need to be called again when the 
+// access token expires in about 1 hour
+async function getAccessToken() {
+    // https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
+    const response = await fetch(`https://accounts.spotify.com/api/token`, {
+        body: new URLSearchParams({
+            'grant_type': 'client_credentials'
+        }),
+        method: `POST`,
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+
+    const parsed = await response.json();
+    return parsed["access_token"];
+}
+
 app.listen(port, () => {
     console.log(`App listening on port http://localhost/${port}`);
 });
